c7d7a9beb1ef8ee764ea8db98071a3cdab39ff53
Ercan Civi <ercancivi@gmail.com>
2021-01-26 17:42:15 -0600

    Added FirstBranchClass01 and FirstBranchClass02

---
+++ src/ercan/FirstBranchClass01.java
@@ -0,0 +1,11 @@
+package ercan;
+
+public class FirstBranchClass01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+    }
+
+}
---
+++ src/ercan/FirstBranchClass02.java
@@ -0,0 +1,11 @@
+package ercan;
+
+public class FirstBranchClass02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Javaholic");
+
+    }
+
+}
