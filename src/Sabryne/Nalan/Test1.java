dd1e528c123b90e84ac97fa156b0ffdd365fbcf1
Nalan <nalanbudak6@gmail.com>
2021-01-26 17:17:55 -0600

    update

--- src/Sabryne/Nalan/Test1.java
+++ src/Sabryne/Nalan/Test1.java
@@ -2,6 +2,6 @@ package Sabryne.Nalan;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello B24");
+        System.out.println("Hello Group24");
     }
 }
