5c5f6460253120d56d9cf68b0851eccdc6ce0160
Ahmed Sdiri <sdiri.ad@gmail.com>
2021-01-26 17:32:52 -0600

    branche b1  and b2

--- src/Ahmed/Med1.java
+++ src/Ahmed/Med1.java
@@ -4,5 +4,6 @@ public class Med1 {
     public static void main(String[] args) {
         System.out.println("welcome to Med's land");
         System.out.println("2020 darth vador I'm your Daddy !!!");
+        //b2
     }
 }
